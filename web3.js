103d2b4a760102f688f4dea7db447b1ac68bdf89
Wathek Khlifi <khlifi.wathek@gmail.com>
2021-03-29 12:47:01 +0100

    fixed web3

--- web3.js
+++ web3.js
@@ -1,27 +1,28 @@
 const Web3 = require('web3');
 const Test = require('./build/contracts/Test.json');
 
-const init = async () => {
-  const web3 = new Web3('http://localhost:8545');
+const provider = new Web3.providers.HttpProvider('http://localhost:8545');
+const web3 = new Web3(provider);
 
+web3.eth.getBlockNumber().then(() => console.log('done !'));
+
+const init = async () => {
   const id = await web3.eth.net.getId();
   const deployedNetwork = Test.networks[id];
+
   const contract = new web3.eth.Contract(Test.abi, deployedNetwork.address);
 
-  const adresses = web3.eth.getAccounts();
-  console.log(adresses[0]);
+  const add = await web3.eth.accounts.wallet.add(
+    '0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63'
+  );
 
   await contract.methods.setValeur(10).send({
-    from: adresses[0],
+    from: add.address,
+    gas: 4600000,
   });
 
   const data = await contract.methods.getValeur().call();
-
   console.log(data);
 };
 
-try {
-  init();
-} catch (error) {
-  console.log(error);
-}
+init();
